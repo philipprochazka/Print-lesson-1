84a65349816d23cdd234e418d00f53ac220408d4
AzureAD\PhilipProcházka <PhilipProchazka@oknadomov.cz>
2020-12-21 11:37:00 +0100

    dsadasd
    asdasdas

---
+++ print_přiběh_nefunguje_nechapu.py
@@ -0,0 +1,19 @@
+def new_variable():
+    new_func()
+
+def new_func():
+    character_name="Honza"
+    character_name1="karel"
+
+
+character_age = "70"
+character_age_1="35"
+print("byl jednou jeden chlapec jmenoval se honza")
+print("bylo mu ""character_age_1")
+print(" Opravdu se mu libilo jmeno " "character_name")
+print("ale, nelibilo se mu ze už mu je " "character_age"".")
+
+print("byl jednou jeden chlapec jmenoval se "+"character_name1"",")
+print("bylo mu character_age_1.")
+print("Opravdu se mu libilo jmeno "+"character_name")
+print("ale, nelibilo se mu, že už mu je ""+ character_age")
